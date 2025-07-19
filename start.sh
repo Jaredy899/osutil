6513842d123578ee02d4bd1659786a4ec786fda8
Jared Cervantes <76603258+Jaredy899@users.noreply.github.com>
2025-07-19 19:18:19 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -35,6 +35,9 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+# Remove quarantine attribute to avoid Gatekeeper warning
+xattr -d com.apple.quarantine "$temp_file" 2>/dev/null || true
+
 "$temp_file" "$@"
 exit_code=$?
 
