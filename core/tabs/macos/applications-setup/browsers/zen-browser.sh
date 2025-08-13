3eeca98f07dce31c2d586385939a86e63b36160b
Jared Cervantes <jared@jaredcervantes.com>
2025-08-12 22:04:00 -0400

    minor fixes

--- core/tabs/macos/applications-setup/browsers/zen-browser.sh
+++ core/tabs/macos/applications-setup/browsers/zen-browser.sh
@@ -3,9 +3,9 @@
 . ../../common-script.sh
 
 installZenBrowser() {
-    if ! brewprogram_exists zen; then
+    if ! brewprogram_exists zen-browser; then
         printf "%b\n" "${YELLOW}Installing Zen Browser...${RC}"
-        if ! brew install --cask zen; then
+        if ! brew install --cask zen-browser; then
             printf "%b\n" "${RED}Failed to install Zen Browser. Please check your Homebrew installation or try again later.${RC}"
             exit 1
         fi
