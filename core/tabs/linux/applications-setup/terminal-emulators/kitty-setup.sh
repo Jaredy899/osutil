1cfe93b011c12ac1e30f0f90736720614fba6b19
Jared Cervantes <jared@jaredcervantes.com>
2026-02-27 14:16:50 -0500

    no config for terminals

--- core/tabs/linux/applications-setup/terminal-emulators/kitty-setup.sh
+++ core/tabs/linux/applications-setup/terminal-emulators/kitty-setup.sh
@@ -25,17 +25,17 @@ installKitty() {
     fi
 }
 
-setupKittyConfig() {
-    printf "%b\n" "${YELLOW}Copying Kitty configuration files...${RC}"
-    if [ -d "${HOME}/.config/kitty" ] && [ ! -d "${HOME}/.config/kitty-bak" ]; then
-        cp -r "${HOME}/.config/kitty" "${HOME}/.config/kitty-bak"
-    fi
-    mkdir -p "${HOME}/.config/kitty/"
-    curl -sSLo "${HOME}/.config/kitty/kitty.conf" https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/kitty/kitty.conf
-    curl -sSLo "${HOME}/.config/kitty/nord.conf" https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/kitty/nord.conf
-}
+# setupKittyConfig() {
+#     printf "%b\n" "${YELLOW}Copying Kitty configuration files...${RC}"
+#     if [ -d "${HOME}/.config/kitty" ] && [ ! -d "${HOME}/.config/kitty-bak" ]; then
+#         cp -r "${HOME}/.config/kitty" "${HOME}/.config/kitty-bak"
+#     fi
+#     mkdir -p "${HOME}/.config/kitty/"
+#     curl -sSLo "${HOME}/.config/kitty/kitty.conf" https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/kitty/kitty.conf
+#     curl -sSLo "${HOME}/.config/kitty/nord.conf" https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/kitty/nord.conf
+# }
 
 checkEnv
 checkEscalationTool
 installKitty
-setupKittyConfig
+# setupKittyConfig
